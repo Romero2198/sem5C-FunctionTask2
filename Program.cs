9459d215826047a8ccc9ef929343e16d331aea60
Yung.Energy <morskojroma@gmail.com>
2022-07-13 22:00:21 +0300

    Task2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int[] numbers = new int [4];
+int count = 0;
+FillArray(numbers);
+PrintArray(numbers);
+
+for(int i = 1; i < numbers.Length; i = i + 2)
+{
+    count = numbers[i] + count;
+}
+Console.WriteLine($"-> {count}");
+
+
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
